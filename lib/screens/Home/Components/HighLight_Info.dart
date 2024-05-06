4166d5f2577d1e23a40075ca733afbd926cc4c60
Admin <maayush235@gmail.com>
2024-05-07 00:53:14 +0530

    Radhey Radhey

---
+++ lib/screens/Home/Components/HighLight_Info.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import '../../../Components/Animated_Coiunter.dart';
+import '../../../constants.dart';
+import 'High_Light.dart';
+
+class High_Light_info extends StatelessWidget {
+  const High_Light_info({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: defaultPadding),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Highlights(counter: AnimatedCounter(
+            value: 119,
+            text: "k+",
+          ),
+            label: 'Subscribers',),
+
+          Highlights(counter: AnimatedCounter(
+            value: 40,
+            text: "+",
+          ),
+            label: 'Videos',),
+
+          Highlights(counter: AnimatedCounter(
+            value: 39,
+            text: "+",
+          ),
+            label: 'GitHub Projects',),
+
+          Highlights(counter: AnimatedCounter(
+            value: 10,
+            text: "k+",
+          ),
+            label: 'Stars',),
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/Home/Home_Screen.dart
+++ lib/screens/Home/Home_Screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:portfolio_website/constants.dart';
+import 'package:portfolio_website/models/Project.dart';
 import 'package:portfolio_website/screens/main/main_Screen.dart';
+import 'Components/HighLight_Info.dart';
 import 'Components/Home_banner.dart';
+import 'Components/Projectcard.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,11 +13,34 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MainScreen(children: [
-      Home_Banner()
+      Home_Banner(),
+      High_Light_info(),
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'My Project',
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+          SizedBox(height: defaultPadding,),
+          GridView.builder(
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            itemCount: demo_projects.length,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                crossAxisSpacing: defaultPadding,
+                childAspectRatio: 1.3,
+                mainAxisSpacing: defaultPadding),
+            itemBuilder: (context, index) => Projectcard(
+              project: demo_projects[index],
+            ),
+          )
+        ],
+      ),
+
     ]);
   }
 }
-
-
 
 
