f129f7f4e280d9298ca23775dfd4951261e3cf36
Admin <maayush235@gmail.com>
2024-05-06 23:47:40 +0530

    Radhey Radhey

--- lib/screens/main/main_Screen.dart
+++ lib/screens/main/main_Screen.dart
@@ -15,6 +15,7 @@ class MainScreen extends StatelessWidget {
         child: Container(
           constraints: BoxConstraints(maxWidth: maxWidth),
           child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Expanded(
                   // this take 2/9 part
